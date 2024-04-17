83e1a9186b76c6db57ed1e1a4a7674a1fe5406d3
bretello <bretello@distruzione.org>
2024-04-17 14:44:09 +0200

    bump fancycompleter to 0.10.5
    fixes #15

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "Topic :: Software Development :: Debuggers",
     ],
     install_requires=[
-        "fancycompleter @ git+https://github.com/bretello/fancycompleter@0.10.4",  # noqa: E501
+        "fancycompleter @ git+https://github.com/bretello/fancycompleter@0.10.5",  # noqa: E501
         "pygments",
     ],
     extras_require={
