b254d5e036e862583540174e069b8c528dcd259b
bretello <bretello@distruzione.org>
2025-04-13 14:49:52 +0200

    deps: install fancycompleter from PyPi
    Following the discussion on https://github.com/bretello/pdbpp/issues/31#issuecomment-2765714724,
    `fancycompleter==0.11.0` is finally released on PyPi

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         "Topic :: Software Development :: Debuggers",
     ],
     install_requires=[
-        "fancycompleter @ git+https://github.com/bretello/fancycompleter@0.10.6",  # noqa: E501
+        "fancycompleter>=0.11.0",
         "pygments",
     ],
     extras_require={
